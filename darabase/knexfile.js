a04c0cd1d25feab86f2d95a17d248695ace45e74
olawale-o <omoogunolawale@gmail.com>
2022-06-25 14:18:55 +0100

    Generate knex config file

---
+++ darabase/knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      database: 'node-redis-cache-dev',
+      user: 'root',
+      password: '',
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migration',
+    },
+    seeds: {
+      directory: './seeds',
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
