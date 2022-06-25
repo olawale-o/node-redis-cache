ebde67874796d6e812c5ab0f52d7228ba2e59441
olawale-o <omoogunolawale@gmail.com>
2022-06-25 14:29:51 +0100

    Create user model

---
+++ darabase/models/User.js
@@ -0,0 +1,12 @@
+class User {
+  constructor(obj) {
+    this.id = obj.id;
+    this.email = obj.email;
+    this.first_name = obj.first_name;
+    this.last_name = Object.last_name;
+    this.created_at = obj.created_at;
+    this.updated_at = obj.updated_at;
+  }
+}
+
+module.exports = User;
